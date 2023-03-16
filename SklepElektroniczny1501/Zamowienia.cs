05c617bcc6067994b5ab9b96f879ed24c5ac94ce
Michał Kowalik <michalkowalik98@gmail.com>
2023-03-16 13:00:17 +0100

    Add project files.

---
+++ SklepElektroniczny1501/Zamowienia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SklepElektroniczny
+{
+    public partial class Zamowienia : Form
+    {
+        public Zamowienia()
+        {
+            InitializeComponent();
+        }
+    }
+}
