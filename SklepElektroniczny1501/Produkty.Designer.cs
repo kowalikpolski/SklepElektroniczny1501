d3a612423fa73c8a3804f8a3deef81905e361e42
Michał Kowalik <michalkowalik98@gmail.com>
2023-03-17 11:23:05 +0100

    Dodanie menu plik z opcjami nowy i edytuj

--- SklepElektroniczny1501/Produkty.Designer.cs
+++ SklepElektroniczny1501/Produkty.Designer.cs
@@ -30,22 +30,27 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.produktBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.produktBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataSetProdukty = new SklepElektroniczny1501.DataSetProdukty();
-            this.dataSetProduktyBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.produktTableAdapter = new SklepElektroniczny1501.DataSetProduktyTableAdapters.produktTableAdapter();
             this.nazwaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kategoriaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.iloscdostepnaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cenaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.produktBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSetProdukty = new SklepElektroniczny1501.DataSetProdukty();
+            this.label1 = new System.Windows.Forms.Label();
+            this.produktBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSetProduktyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.produktTableAdapter = new SklepElektroniczny1501.DataSetProduktyTableAdapters.produktTableAdapter();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edytujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetProdukty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetProduktyBindingSource)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -67,35 +72,8 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(941, 662);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
             this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Lista produktów:";
-            // 
-            // produktBindingSource
-            // 
-            this.produktBindingSource.DataMember = "produkt";
-            this.produktBindingSource.DataSource = this.dataSetProdukty;
-            // 
-            // dataSetProdukty
-            // 
-            this.dataSetProdukty.DataSetName = "DataSetProdukty";
-            this.dataSetProdukty.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dataSetProduktyBindingSource
-            // 
-            this.dataSetProduktyBindingSource.DataSource = this.dataSetProdukty;
-            this.dataSetProduktyBindingSource.Position = 0;
-            // 
-            // produktTableAdapter
-            // 
-            this.produktTableAdapter.ClearBeforeFill = true;
             // 
             // nazwaDataGridViewTextBoxColumn
             // 
@@ -137,6 +115,68 @@
             this.cenaDataGridViewTextBoxColumn.ReadOnly = true;
             this.cenaDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
+            // produktBindingSource
+            // 
+            this.produktBindingSource.DataMember = "produkt";
+            this.produktBindingSource.DataSource = this.dataSetProdukty;
+            // 
+            // dataSetProdukty
+            // 
+            this.dataSetProdukty.DataSetName = "DataSetProdukty";
+            this.dataSetProdukty.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista produktów:";
+            // 
+            // dataSetProduktyBindingSource
+            // 
+            this.dataSetProduktyBindingSource.DataSource = this.dataSetProdukty;
+            this.dataSetProduktyBindingSource.Position = 0;
+            // 
+            // produktTableAdapter
+            // 
+            this.produktTableAdapter.ClearBeforeFill = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(965, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
+            this.edytujToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(38, 20);
+            this.toolStripMenuItem1.Text = "Plik";
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // edytujToolStripMenuItem
+            // 
+            this.edytujToolStripMenuItem.Enabled = false;
+            this.edytujToolStripMenuItem.Name = "edytujToolStripMenuItem";
+            this.edytujToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.edytujToolStripMenuItem.Text = "Edytuj";
+            this.edytujToolStripMenuItem.Click += new System.EventHandler(this.edytujToolStripMenuItem_Click);
+            // 
             // Produkty
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -144,14 +184,18 @@
             this.ClientSize = new System.Drawing.Size(965, 748);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Produkty";
             this.Text = "Produkty";
             this.Load += new System.EventHandler(this.Produkty_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetProdukty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.produktBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSetProduktyBindingSource)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +215,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn kategoriaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn iloscdostepnaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn cenaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edytujToolStripMenuItem;
     }
 }
 
--- SklepElektroniczny1501/Produkty.cs
+++ SklepElektroniczny1501/Produkty.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SklepElektroniczny1501
 {
@@ -32,9 +34,32 @@ namespace SklepElektroniczny1501
                 var name = dataGridView1.Rows[selectedItem].Cells[0].Value.ToString();
                 var model = dataGridView1.Rows[selectedItem].Cells[1].Value.ToString();
                 Form produkt = new ProduktyEdycja(name,model);
-                produkt.Show();
+                produkt.ShowDialog();
             }
            
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells == null)
+                edytujToolStripMenuItem.Enabled = false;
+            else
+                edytujToolStripMenuItem.Enabled = true;
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form produkt = new ProduktyEdycja(null, null);
+            produkt.ShowDialog();
+        }
+
+        private void edytujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var selectedItem = dataGridView1.SelectedCells[0].RowIndex;
+            var name = dataGridView1.Rows[selectedItem].Cells[0].Value.ToString();
+            var model = dataGridView1.Rows[selectedItem].Cells[1].Value.ToString();
+            Form produkt = new ProduktyEdycja(name, model);
+            produkt.ShowDialog();
         }
     }
 }
