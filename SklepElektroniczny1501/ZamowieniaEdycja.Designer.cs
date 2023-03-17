d99eb63c2aec22d9e5ca6828d32d3df9bec98969
Michał Kowalik <michalkowalik98@gmail.com>
2023-03-17 15:47:13 +0100

    Implementacja wyświetlania listy produktów w zamówieniu i generowania numeru zamówienia w nowym zamówieniu

--- SklepElektroniczny1501/ZamowieniaEdycja.Designer.cs
+++ SklepElektroniczny1501/ZamowieniaEdycja.Designer.cs
@@ -33,7 +33,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.labelOrder = new System.Windows.Forms.Label();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaPozycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edytujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -65,34 +72,96 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(829, 571);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CurrentCellChanged += new System.EventHandler(this.dataGridView1_CurrentCellChanged);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 7;
             this.label1.Text = "Numer zamówienia:";
+            // 
+            // labelOrder
+            // 
+            this.labelOrder.AutoSize = true;
+            this.labelOrder.Location = new System.Drawing.Point(117, 34);
+            this.labelOrder.Name = "labelOrder";
+            this.labelOrder.Size = new System.Drawing.Size(0, 13);
+            this.labelOrder.TabIndex = 8;
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Location = new System.Drawing.Point(105, 624);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(0, 13);
+            this.labelSum.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(853, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaPozycjaToolStripMenuItem,
+            this.edytujToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // nowaPozycjaToolStripMenuItem
+            // 
+            this.nowaPozycjaToolStripMenuItem.Name = "nowaPozycjaToolStripMenuItem";
+            this.nowaPozycjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowaPozycjaToolStripMenuItem.Text = "Nowa pozycja";
+            this.nowaPozycjaToolStripMenuItem.Click += new System.EventHandler(this.nowaPozycjaToolStripMenuItem_Click);
+            // 
+            // edytujToolStripMenuItem
+            // 
+            this.edytujToolStripMenuItem.Enabled = false;
+            this.edytujToolStripMenuItem.Name = "edytujToolStripMenuItem";
+            this.edytujToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.edytujToolStripMenuItem.Text = "Edytuj";
+            this.edytujToolStripMenuItem.Click += new System.EventHandler(this.edytujToolStripMenuItem_Click);
             // 
             // ZamowieniaEdycja
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 692);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.labelOrder);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "ZamowieniaEdycja";
             this.Text = "Dodawanie/Edycja Zamówienia";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,5 +174,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOrder;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowaPozycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edytujToolStripMenuItem;
     }
 }
--- SklepElektroniczny1501/ZamowieniaEdycja.cs
+++ SklepElektroniczny1501/ZamowieniaEdycja.cs
@@ -1,20 +1,121 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Linq;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SklepElektroniczny1501
 {
+    
     public partial class ZamowieniaEdycja : Form
     {
-        public ZamowieniaEdycja()
+        private IEnumerable orderList;
+        private DataContext dc; 
+        private string orderNr;
+        public ZamowieniaEdycja(string order)
         {
             InitializeComponent();
+            
+            dc = new DataContext("Data Source=localhost\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True");
+            Table<produkt> produkt = dc.GetTable<produkt>();
+            Table<zamowienie_produkt> zamowienie_produkt = dc.GetTable<zamowienie_produkt>();
+            Table<zamowienie> zamowienie = dc.GetTable<zamowienie>();
+            if(order != null)
+            {
+                var orderItems = (from zp in zamowienie_produkt
+                             join prod in produkt on zp.id_produkt equals prod.id
+                             join zam in zamowienie on zp.id_zamowienie equals zam.id
+                             where zam.numer_zamowienia == order
+                             select new
+                             {
+                                 prod.nazwa,
+                                 prod.model,
+                                 zp.ilosc,
+                                 zp.cena,
+                             }).ToList();
+                dataGridView1.DataSource = orderItems;
+                orderNr = order;
+                labelSum.Text=orderItems.Sum(x=>x.cena).ToString();
+            }
+            else
+            {
+                labelSum.Text = "0";
+                var query = (from zam in zamowienie
+                              group zam by true into r
+                              select new { maxOrder = r.Max(x => x.numer_zamowienia) }).ToList()[0];
+                var str=query.maxOrder.Split('/');
+                if (str[0] == DateTime.Now.Year.ToString())
+                {
+                    str[1] = String.Format("{0:000}", int.Parse(str[1]) + 1);
+                    orderNr = str[0] + "/" + str[1];
+                }
+                else
+                {
+                    str[0] = DateTime.Now.Year.ToString();
+                    str[1] = "001";
+                }
+                    orderNr = str[0] + "/" + str[1];
+                    
+            }
+            labelOrder.Text = orderNr;
+
+
+            /*SELECT        produkt.nazwa, produkt.model, zamowienie_produkt.ilosc, zamowienie_produkt.cena
+FROM            zamowienie_produkt INNER JOIN
+                         produkt ON zamowienie_produkt.id_produkt = produkt.id*/
+        }
+
+        private void nowaPozycjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+                
+               
+                Form zamowieniePozycjeEdycja = new ZamowieniePozycjeEdycja(null,null);
+                zamowieniePozycjeEdycja.ShowDialog();
+                this.Close();
+            
+        }
+
+        private void edytujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells != null)
+            {
+                var selectedItem = dataGridView1.SelectedCells[0].RowIndex;
+                var name = dataGridView1.Rows[selectedItem].Cells[0].Value.ToString();
+                var model = dataGridView1.Rows[selectedItem].Cells[1].Value.ToString();
+                Form zamowieniePozycjeEdycja = new ZamowieniePozycjeEdycja(name, model);
+                zamowieniePozycjeEdycja.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells != null)
+            {
+                var selectedItem = dataGridView1.SelectedCells[0].RowIndex;
+                var name = dataGridView1.Rows[selectedItem].Cells[0].Value.ToString();
+                var model = dataGridView1.Rows[selectedItem].Cells[1].Value.ToString();
+                Form zamowieniePozycjeEdycja = new ZamowieniePozycjeEdycja(name, model);
+                zamowieniePozycjeEdycja.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells == null)
+                edytujToolStripMenuItem.Enabled = false;
+            else
+                edytujToolStripMenuItem.Enabled = true;
         }
     }
 }
