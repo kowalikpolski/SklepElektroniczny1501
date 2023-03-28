88d7fc1bfc2e82dd1e1c87f89a889ade01f7973f
hardkormorswin <michalkowalik98@gmail.com>
2023-03-28 15:27:54 +0200

    stworzenie klas walidujących

---
+++ SklepElektroniczny1501/OrderProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepElektroniczny1501
+{
+    public class OrderProductValidator : AbstractValidator<zamowienie_produkt>
+    {
+        public OrderProductValidator()
+        {
+            RuleFor(zamowienie_produkt => zamowienie_produkt.ilosc).GreaterThan(0).WithMessage("Liczba zamawianych produktów musi być dodatnia");
+            RuleFor(zamowienie_produkt => zamowienie_produkt.cena).GreaterThan(0).WithMessage("Cena zamawianych produktów musi być liczbą dodatnią");
+        }
+    }
+}
---
+++ SklepElektroniczny1501/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepElektroniczny1501
+{
+    public class ProductValidator : AbstractValidator<produkt>
+    {
+        public ProductValidator()
+        { 
+            RuleFor(produkt=>produkt.model).NotEmpty().WithMessage("Wypełnij pole model");
+            RuleFor(produkt=>produkt.nazwa).NotEmpty().WithMessage("Wypełnij pole nazwa");
+            RuleFor(produkt=>produkt.ilosc_dostepna).GreaterThan(-1).WithMessage("Dostępna ilość produktów musi być liczbą nieujemną");
+            RuleFor(produkt=>produkt.cena).GreaterThan(-1).WithMessage("Cena produktu musi być liczbą nieujemną");   
+        }
+    }
+}
